e9b7832ec035bdd5d52d5304c187005e1b9fc01e
Valentine <valazom@gmail.com>
2023-10-05 12:30:44 +0200

    upddate order dto

--- Order.API/Dto/UserOrderHistoryRecord.cs
+++ Order.API/Dto/UserOrderHistoryRecord.cs
@@ -8,7 +8,7 @@ namespace Order.API.Dto;
 public record UserOrderHistoryRecord {
     [EnumDataType (typeof (OrderStatus))]
     [JsonConverter (typeof (StringEnumConverter))]
-    public OrderStatus OrderStatus { get; set; }
+    public OrderStatus? OrderStatus { get; set; }
     public DateTime? StartDate { get; set; }
     public DateTime? EndDate { get; set; }
     public int Page { get; set; } = 1;
